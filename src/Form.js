a46de475aa1bf71e2c6358ce5b3f1b11b0b3a202
Christopher Spooner <christopherspooner423@gmail.com>
2023-05-22 13:18:02 -0500

    update

--- src/Form.js
+++ src/Form.js
@@ -8,26 +8,31 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row'
 import axios from 'axios';
 import {useState} from 'react';
+import Col from 'react-bootstrap/Col';
+import './App.css';
 
 function ExploreForm() {
-    let userinput = ""
+    const [userinput, setUserInput] = useState("");
+    const [image, setImage] = useState("");
     const [citydata, setCitydata] = useState({
-        display_name: "Paris, Ile-de-France, Metropolitan France, France",
-        lat: "48.8588897",
-        lon: "2.3200410217200766"
+        display_name: "",
+        lat: "",
+        lon: "",
     })
     return (
-        <Form>
+        <Form id="cityForm">
             <Row>
                 <Form.Group>
-                    <Form.Label>City</Form.Label>
-                    <Form.Control onChange={function(event){
-                    userinput = event.target.value
+                    <Col><Form.Label className='text' id='title'>City</Form.Label></Col>
+                    <Form.Control id='searchbar' onChange={function(event){
+                    setUserInput(event.target.value);
                     }} placeholder="Search City" />
 
                     <Button onClick={function(){
                         console.log(userinput)
-                         let response = axios.get(`https://us1.locationiq.com/v1/search?key=pk.e7ac28688556d9f05c9e6e2517927329&q=${userinput}&format=json`);
+                         let response = axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_APT_KEY}&q=${userinput}&format=json`);
+                         
+
                          response.then(function(res){
                              // console.log(res.data)
                              console.log(res.data[0])
@@ -37,10 +42,13 @@ function ExploreForm() {
                     }}>Explore!</Button>
                 </Form.Group>
             </Row>
-            <h1>{citydata.display_name}</h1>
-            <h1>{citydata.lon}</h1>
-            <h1>{citydata.lat}</h1>
-        </Form>
+            <h1 className='info text'>{citydata.display_name}</h1>
+            <div id='coor'>
+            <h1 className='info text coordinates' id='long'>Longitude: {citydata.lon}</h1>
+            <h1 className='info text coordinates' id='lati'>Latitude: {citydata.lat}</h1>
+            </div>
+            <img id="Mapp"alt="City Map" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_APT_KEY}&center=${citydata.lat},${citydata.lon}&zoom=13&format=jpg`}  />
+            </Form>
     )
 }
 
